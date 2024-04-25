ede430c49f3750dee67c08c2ec752fed71c6ee36
atian155 <atianthony@outlook.com>
2024-04-25 15:14:24 +0300

    Add 2D Character Example

--- frontend/app/(user)/boarding/page.tsx
+++ frontend/app/(user)/boarding/page.tsx
@@ -1,4 +1,11 @@
-import Eye1 from "../2d/eye/eye1.svg"
+import Eye1 from "../2d/eye/eye5.svg";
+import Nose from "../2d/nose/nose4.svg";
+import Body1 from "../2d/body/female_body1.svg";
+import Face1 from "../2d/face/face1.svg";
+import Mouth from "../2d/mouth/mouth6.svg";
+import Hair from "../2d/hair/hair2.svg";
+import HairBg from "../2d/hair/hair2_backdrop.svg";
+import Eyebrow from "../2d/eyebrow/eyebrow1.svg";
 
 const Boarding = async () => {
   return (
@@ -11,10 +18,29 @@ const Boarding = async () => {
         </p>
       </section>
 
-      <div className="w-full bg-slate-100 mt-4 rounded-t-xl flex justify-end gap-8">
-        <div></div>
-        <div className="">
-          <Eye1 />
+      <div className="w-full h-full bg-slate-100 mt-4 rounded-t-xl flex gap-8">
+        <div className="relative">
+          <HairBg width={180} className="absolute left-[97px] -top-[2px] z-0" />
+          <Face1 width={160} className="absolute left-1/2 -translate-x-1/2" />
+          <Hair width={200} className="absolute left-[58px] -top-[10px]" />
+
+          <Eyebrow
+            width={112}
+            className="absolute left-1/2 -translate-x-1/2 top-[51px]"
+          />
+          <Eye1
+            width={102}
+            className="absolute left-1/2 -translate-x-1/2 top-[71px]"
+          />
+          <Nose
+            width={32}
+            className="absolute left-1/2 -translate-x-1/2 top-[98px]"
+          />
+          <Mouth
+            width={44}
+            className="absolute left-1/2 -translate-x-1/2 top-[126px]"
+          />
+          <Body1 width={360} className="mt-[144px]" />
         </div>
       </div>
     </>
