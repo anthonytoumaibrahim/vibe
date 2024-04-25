a8dcb617b459723f37f29e548001fd70ec7cc4df
atian155 <atianthony@outlook.com>
2024-04-25 11:11:05 +0300

    Update Middleware to include Landing Page

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -3,10 +3,10 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   if (request.cookies.has("token")) {
-    return NextResponse.redirect(new URL("/", request.url));
+    return NextResponse.redirect(new URL("/home", request.url));
   }
 }
 
 export const config = {
-  matcher: ["/login", "/signup"],
+  matcher: ["/login", "/signup", "/"],
 };
