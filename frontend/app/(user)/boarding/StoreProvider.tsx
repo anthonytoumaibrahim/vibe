e75c709f6e96550c96653ea769cbd966b935c60b
atian155 <atianthony@outlook.com>
2024-04-28 10:29:37 +0300

    Add 2D body thumbnails

--- frontend/app/(user)/boarding/StoreProvider.tsx
+++ frontend/app/(user)/boarding/StoreProvider.tsx
@@ -14,45 +14,75 @@ export default function StoreProvider({
   const storeRef = useRef<AppStore>();
   if (!storeRef.current) {
     storeRef.current = makeStore();
-    if (typeof window !== "undefined") {
-      const localData = JSON.parse(
-        localStorage.getItem("vibe_2d_boarding") ?? "{}"
-      );
-      if (Object.keys(localData).length > 0) {
-        storeRef.current.dispatch(initializeData(localData));
-      } else {
-        storeRef.current.dispatch(
-          initializeData({
-            body: {
-              id: 1,
-              fill: "#df9777",
-            },
-            face: {
-              id: 1,
-            },
-            hair: {
-              id: 1,
-              fill: "#c73030",
-            },
-            eye: {
-              id: 5,
-              fill: "#72A0C1",
-            },
-            eyebrow: {
-              id: 5,
-              fill: "#c73030",
-            },
-            nose: {
-              id: 3,
-            },
-            mouth: {
-              id: 6,
-              fill: "#ed7172",
-            },
-          })
-        );
-      }
-    }
+    storeRef.current.dispatch(
+      initializeData({
+        body: {
+          id: 1,
+          fill: "#df9777",
+        },
+        face: {
+          id: 1,
+        },
+        hair: {
+          id: 1,
+          fill: "#c73030",
+        },
+        eye: {
+          id: 5,
+          fill: "#72A0C1",
+        },
+        eyebrow: {
+          id: 5,
+          fill: "#c73030",
+        },
+        nose: {
+          id: 3,
+        },
+        mouth: {
+          id: 6,
+          fill: "#ed7172",
+        },
+      })
+    );
+    // if (typeof window !== "undefined") {
+    //   const localData = JSON.parse(
+    //     localStorage.getItem("vibe_2d_boarding") ?? "{}"
+    //   );
+    //   if (Object.keys(localData).length > 0) {
+    //     storeRef.current.dispatch(initializeData(localData));
+    //   } else {
+    //     storeRef.current.dispatch(
+    //       initializeData({
+    //         body: {
+    //           id: 1,
+    //           fill: "#df9777",
+    //         },
+    //         face: {
+    //           id: 1,
+    //         },
+    //         hair: {
+    //           id: 1,
+    //           fill: "#c73030",
+    //         },
+    //         eye: {
+    //           id: 5,
+    //           fill: "#72A0C1",
+    //         },
+    //         eyebrow: {
+    //           id: 5,
+    //           fill: "#c73030",
+    //         },
+    //         nose: {
+    //           id: 3,
+    //         },
+    //         mouth: {
+    //           id: 6,
+    //           fill: "#ed7172",
+    //         },
+    //       })
+    //     );
+    //   }
+    // }
   }
 
   return <Provider store={storeRef.current}>{children}</Provider>;
