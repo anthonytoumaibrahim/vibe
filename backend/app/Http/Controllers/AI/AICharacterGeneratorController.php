d341063fb99777c56f9a6ad87b9f17b3e3e51627
atian155 <atianthony@outlook.com>
2024-05-03 17:07:35 +0300

    (Backend) Add AI Descriptions to 2D Parts

---
+++ backend/app/Http/Controllers/AI/AICharacterGeneratorController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\AI;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AICharacterGeneratorController extends Controller
+{
+    public function generate(Request $request)
+    {
+    }
+}
--- backend/database/seeders/CharacterPartSeeder.php
+++ backend/database/seeders/CharacterPartSeeder.php
@@ -17,139 +17,139 @@ class CharacterPartSeeder extends Seeder
         CharacterPart::create([
             'client_id' => 1,
             'type' => 'body',
-            'ai_description' => '',
+            'ai_description' => 'Woman\'s body, curvy shape',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 2,
             'type' => 'body',
-            'ai_description' => '',
+            'ai_description' => 'Man\'s muscly body',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 3,
             'type' => 'body',
-            'ai_description' => '',
+            'ai_description' => 'Unisex body, more feminine',
             'default' => true
         ]);
         // Face
         CharacterPart::create([
             'client_id' => 1,
             'type' => 'face',
-            'ai_description' => '',
+            'ai_description' => 'Round face, feminine appearance',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 2,
             'type' => 'face',
-            'ai_description' => '',
+            'ai_description' => 'Oval face shape, masculine, with skin details',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 3,
             'type' => 'face',
-            'ai_description' => '',
+            'ai_description' => 'Oval face shape, masculine appearance',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 4,
             'type' => 'face',
-            'ai_description' => '',
+            'ai_description' => 'Round face, slighly large, feminine',
             'default' => true
         ]);
         // Hair
         CharacterPart::create([
             'client_id' => 1,
             'type' => 'hair',
-            'ai_description' => '',
+            'ai_description' => 'Short straight feminine hair',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 2,
             'type' => 'hair',
-            'ai_description' => '',
+            'ai_description' => 'Short frizzy hairstyle, feminine',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 3,
             'type' => 'hair',
-            'ai_description' => '',
+            'ai_description' => 'Tall feminine hair behind back',
             'price' => 100
         ]);
         CharacterPart::create([
             'client_id' => 10,
             'type' => 'hair',
-            'ai_description' => '',
+            'ai_description' => 'Tall female hair in a bun',
             'price' => 100
         ]);
         CharacterPart::create([
             'client_id' => 6,
             'type' => 'hair',
-            'ai_description' => '',
+            'ai_description' => 'Short female hair in a bun',
             'default' => true,
             'premium' => true
         ]);
         CharacterPart::create([
             'client_id' => 7,
             'type' => 'hair',
-            'ai_description' => '',
+            'ai_description' => 'Medium female straight hair with bangs',
             'default' => true,
             'premium' => true
         ]);
         CharacterPart::create([
             'client_id' => 8,
             'type' => 'hair',
-            'ai_description' => '',
+            'ai_description' => 'Medium wavy female hair',
             'default' => true,
             'premium' => true
         ]);
         CharacterPart::create([
             'client_id' => 4,
             'type' => 'hair',
-            'ai_description' => '',
+            'ai_description' => 'Male hairstyle, with hair strands',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 5,
             'type' => 'hair',
-            'ai_description' => '',
+            'ai_description' => 'Male hairstyle, faded on the sides',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 9,
             'type' => 'hair',
-            'ai_description' => '',
+            'ai_description' => 'Male hairstyle',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 12,
             'type' => 'hair',
-            'ai_description' => '',
+            'ai_description' => 'Short male hair with bun',
             'price' => 100
         ]);
         CharacterPart::create([
             'client_id' => 13,
             'type' => 'hair',
-            'ai_description' => '',
+            'ai_description' => 'Short male hair',
             'price' => 100
         ]);
         CharacterPart::create([
             'client_id' => 14,
             'type' => 'hair',
-            'ai_description' => '',
+            'ai_description' => 'Short messy male hair',
             'price' => 100
         ]);
         CharacterPart::create([
             'client_id' => 15,
             'type' => 'hair',
-            'ai_description' => '',
+            'ai_description' => 'Short, formal male hair',
             'premium' => true,
             'default' => true,
         ]);
         CharacterPart::create([
             'client_id' => 16,
             'type' => 'hair',
-            'ai_description' => '',
+            'ai_description' => 'Medium-tall male hair',
             'premium' => true,
             'default' => true,
         ]);
@@ -157,173 +157,173 @@ class CharacterPartSeeder extends Seeder
         CharacterPart::create([
             'client_id' => 1,
             'type' => 'eyebrow',
-            'ai_description' => '',
+            'ai_description' => 'Medium eyebrow, slightly angry expression',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 2,
             'type' => 'eyebrow',
-            'ai_description' => '',
+            'ai_description' => 'Thick eyebrow, angry expression',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 3,
             'type' => 'eyebrow',
-            'ai_description' => '',
+            'ai_description' => 'Thin eyebrow, innocent expression',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 4,
             'type' => 'eyebrow',
-            'ai_description' => '',
+            'ai_description' => 'Thick eyebrow, normal expression',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 5,
             'type' => 'eyebrow',
-            'ai_description' => '',
+            'ai_description' => 'Thick eyebrow with slightly bent shape',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 6,
             'type' => 'eyebrow',
-            'ai_description' => '',
+            'ai_description' => 'Regular eyebrow shape',
             'default' => true
         ]);
         // Eyes
         CharacterPart::create([
             'client_id' => 1,
             'type' => 'eye',
-            'ai_description' => '',
+            'ai_description' => 'Happy expression',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 2,
             'type' => 'eye',
-            'ai_description' => '',
+            'ai_description' => 'Widely spaced eyes',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 3,
             'type' => 'eye',
-            'ai_description' => '',
+            'ai_description' => 'Lazy eye shape',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 4,
             'type' => 'eye',
-            'ai_description' => '',
+            'ai_description' => 'Smaller eye shape',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 5,
             'type' => 'eye',
-            'ai_description' => '',
+            'ai_description' => 'Asian eye shape',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 6,
             'type' => 'eye',
-            'ai_description' => '',
+            'ai_description' => 'Normal eye shape, slightly closed',
             'default' => true
         ]);
         // Nose
         CharacterPart::create([
             'client_id' => 1,
             'type' => 'nose',
-            'ai_description' => '',
+            'ai_description' => 'Tall nose',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 2,
             'type' => 'nose',
-            'ai_description' => '',
+            'ai_description' => 'Big, crooked rounded nose',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 3,
             'type' => 'nose',
-            'ai_description' => '',
+            'ai_description' => 'Triangle shaped nose',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 4,
             'type' => 'nose',
-            'ai_description' => '',
+            'ai_description' => 'Small, rounded nose',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 5,
             'type' => 'nose',
-            'ai_description' => '',
+            'ai_description' => 'Tall, flat shaped nose',
             'default' => true
         ]);
         // Mouth
         CharacterPart::create([
             'client_id' => 1,
             'type' => 'mouth',
-            'ai_description' => '',
+            'ai_description' => 'Small lips, smiling with a little teeth showing',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 2,
             'type' => 'mouth',
-            'ai_description' => '',
+            'ai_description' => 'Small lips, slight smile',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 3,
             'type' => 'mouth',
-            'ai_description' => '',
+            'ai_description' => 'Small lips, sad expression',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 4,
             'type' => 'mouth',
-            'ai_description' => '',
+            'ai_description' => 'Medium lips, smiling with teeth showing',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 5,
             'type' => 'mouth',
-            'ai_description' => '',
+            'ai_description' => 'Medium lips, relaxed and happy',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 6,
             'type' => 'mouth',
-            'ai_description' => '',
+            'ai_description' => 'Medium lips, relaxed and neutral expression',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 7,
             'type' => 'mouth',
-            'ai_description' => '',
+            'ai_description' => 'Smiling, full teeth showing',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 8,
             'type' => 'mouth',
-            'ai_description' => '',
+            'ai_description' => 'Smiling, slight teeth showing',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 9,
             'type' => 'mouth',
-            'ai_description' => '',
+            'ai_description' => 'Big bottom lip, slight smile',
             'default' => true
         ]);
         CharacterPart::create([
             'client_id' => 10,
             'type' => 'mouth',
-            'ai_description' => '',
+            'ai_description' => 'Small lips, neutral expression',
             'default' => true
         ]);
         // Eyeglasses
         CharacterPart::create([
             'client_id' => 1,
             'type' => 'eyeglasses',
-            'ai_description' => '',
+            'ai_description' => 'Rounded eyeglasses, thin frame',
             'default' => true
         ]);
     }
