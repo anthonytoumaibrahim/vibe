c11c3248a3f67117a44dee9f70f0ddab203a4b26
atian155 <atianthony@outlook.com>
2024-05-03 23:52:35 +0300

    (Backend) Update Posts Migration

--- backend/database/migrations/2024_05_03_204028_create_posts_table.php
+++ backend/database/migrations/2024_05_03_204028_create_posts_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('content', 450);
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
         });
     }
 
