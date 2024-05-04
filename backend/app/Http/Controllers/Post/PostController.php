7b01742fcbc5486f7f3e55c128d93850295f084e
atian155 <atianthony@outlook.com>
2024-05-04 19:40:53 +0300

    (Backend) Edit Post create function to accept images

--- backend/app/Http/Controllers/Post/PostController.php
+++ backend/app/Http/Controllers/Post/PostController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers\Post;
 
-use App\Http\Controllers\Controller;
 use App\Models\Post;
+use App\Models\Image;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\File;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -26,13 +31,38 @@ class PostController extends Controller
 
     public function create(Request $request)
     {
-        $request->validate([
-            'content' => 'required|min:4|max:6000'
+        $validator = Validator::make($request->all(), [
+            'content' => 'required|min:4|max:6000',
+            'images' => [
+                'max:4',
+                File::image()->max('6mb')
+            ]
         ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Your post could not be created'
+            ]);
+        }
+
+        $validated = $validator->validated();
+
         $post = new Post();
-        $post->content = $request->content;
+        $post->content = $validated['content'];
         $post->user_id = Auth::id();
         $post->saveOrFail();
+
+        // Add images
+        $images = $validated['images'];
+        foreach ($images as $image) {
+            // Store
+            $filename = Str::random(12) . "." . $image->extension();
+            Storage::disk('public')->putFileAs("/post_images", $image, $filename);
+            $newImg = new Image();
+            $newImg->url = "/post_images/{$filename}";
+            $post->images()->save($newImg);
+        }
 
         return response()->json([
             'success' => true,
