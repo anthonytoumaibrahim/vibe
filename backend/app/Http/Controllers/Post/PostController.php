b4473bfeb31e743975b1f806a931eb01656a1be2
atian155 <atianthony@outlook.com>
2024-05-04 21:31:16 +0300

    (Backend) Fix create function in PostController

--- backend/app/Http/Controllers/Post/PostController.php
+++ backend/app/Http/Controllers/Post/PostController.php
@@ -31,38 +31,32 @@ class PostController extends Controller
 
     public function create(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'content' => 'required|min:4|max:6000',
-            'images' => [
-                'max:4',
-                File::image()->max('6mb')
-            ]
+        $request->validate([
+            'content' => 'required|min:4|max:6000'
         ]);
 
-        if ($validator->fails()) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Your post could not be created'
-            ]);
-        }
-
-        $validated = $validator->validated();
-
         $post = new Post();
-        $post->content = $validated['content'];
+        $post->content = $request->content;
         $post->user_id = Auth::id();
         $post->saveOrFail();
 
         // Add images
-        $images = $validated['images'];
-        foreach ($images as $image) {
-            // Store
-            $filename = Str::random(12) . "." . $image->extension();
-            Storage::disk('public')->putFileAs("/post_images", $image, $filename);
-            $newImg = new Image();
-            $newImg->url = "/post_images/{$filename}";
-            $post->images()->save($newImg);
+        if ($request->hasFile('images')) {
+
+            $images = $request->images;
+            foreach ($images as $image) {
+                $size = $image->getSize();
+                if ($size <= 6291456) {
+                    // Store
+                    $filename = Str::random(12) . "." . $image->getClientOriginalExtension();
+                    Storage::disk('public')->putFileAs("/post_images", $image, $filename);
+                    $newImg = new Image();
+                    $newImg->url = "/post_images/{$filename}";
+                    $post->images()->save($newImg);
+                }
+            }
         }
+
 
         return response()->json([
             'success' => true,
