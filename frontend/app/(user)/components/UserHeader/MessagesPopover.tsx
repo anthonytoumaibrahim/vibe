d982526089fa57f58823f46897063d5ac4f1f35b
atian155 <atianthony@outlook.com>
2024-05-07 11:14:36 +0300

    (Frontend) Fix Popover Positions on Header

--- frontend/app/(user)/components/UserHeader/MessagesPopover.tsx
+++ frontend/app/(user)/components/UserHeader/MessagesPopover.tsx
@@ -10,7 +10,7 @@ const MessagesPopover = () => {
         <FaMessage size={24} />
       </Popover.Button>
 
-      <Popover.Panel className="absolute translate-y-2 top-full w-max -translate-x-1/3 z-50 bg-white dark:bg-black py-4 px-6 rounded-lg shadow-lg flex flex-col gap-2 before:w-0 before:h-0 before:absolute before:bottom-full before:left-1/2 before:-translate-x-1/2 before:border-l-[8px] before:border-l-transparent before:border-b-[10px] before:border-b-white dark:before:border-b-black before:border-r-[8px] before:border-r-transparent">
+      <Popover.Panel className="absolute translate-y-2 top-full left-1/2 -translate-x-1/2 w-max z-50 bg-white dark:bg-black py-4 px-6 rounded-lg shadow-lg flex flex-col gap-2 before:w-0 before:h-0 before:absolute before:bottom-full before:left-1/2 before:-translate-x-1/2 before:border-l-[8px] before:border-l-transparent before:border-b-[10px] before:border-b-white dark:before:border-b-black before:border-r-[8px] before:border-r-transparent">
         Messages go here
       </Popover.Panel>
     </Popover>
