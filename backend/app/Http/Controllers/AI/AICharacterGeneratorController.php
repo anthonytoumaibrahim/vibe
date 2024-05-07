cf0832dc34f87c67283a5d3e95a4a13fb1ab18ea
atian155 <atianthony@outlook.com>
2024-05-07 22:15:25 +0300

    (Backend) Update OpenAI Request Body

--- backend/app/Http/Controllers/AI/AICharacterGeneratorController.php
+++ backend/app/Http/Controllers/AI/AICharacterGeneratorController.php
@@ -11,6 +11,19 @@ class AICharacterGeneratorController extends Controller
 {
     public function generate(Request $request)
     {
+        $colors = config('character_builder.colors');
+        $parts = CharacterPart::all()->groupBy('type')->map(function ($items, $type) use ($colors) {
+            return [
+                'colors' => $colors[$type] ?? null,
+                'parts' => $items->map(function ($part) {
+                    return [
+                        'id' => $part->client_id,
+                        'description' => $part->ai_description
+                    ];
+                })
+            ];
+        });
+
         $request->validate([
             'message' => 'required|min:16|max:255'
         ]);
@@ -22,7 +35,7 @@ class AICharacterGeneratorController extends Controller
             'messages' => [
                 [
                     'role' => 'system',
-                    'content' => 'You are a helpful 2D character creation assistant designed to output JSON. Your job is to build a 2D character using SVGs that have IDs and predefined colors, based on the user\'s description. The data at your disposal is the following, make sure to use the ai_description field to understand what each part looks like: ' . CharacterPart::all()->makeHidden(['created_at', 'updated_at', 'default', 'premium', 'price', 'server_id'])->groupBy('type') . '. The corresponding HEX colors for each part category are as following: ' . json_encode(config('character_builder.colors')) . '. Your job is to return a JSON object that contains every category of parts, with only one ID and fill color. For example: {"eye": {"id": 4, "fill": "#9a9b9d"}, "body": {"id": 1, "fill": "#f0b8a0"}, "face": {"id": 1}, "hair": {"id": 2, "fill": "#9a3300"}, "nose": {"id": 4}, "mouth": {"id": 8, "fill": "#a31a57"}, "eyebrow": {"id": 4, "fill": "#9a3300"}, "eyeglasses": {"id": 0}}.'
+                    'content' => 'You are a helpful 2D character creation assistant designed to output JSON. Your job is to build a 2D character using SVGs that have IDs and predefined colors, based on the user\'s description. The data at your disposal is the following, make sure to use the description field to understand what each part looks like: ' . $parts . '. Your job is to return a JSON object that contains every type of parts, with only one ID and fill color. For example: {"eye": {"id": 4, "fill": "#9a9b9d"}, "body": {"id": 1, "fill": "#f0b8a0"}, "face": {"id": 1}, "hair": {"id": 2, "fill": "#9a3300"}, "nose": {"id": 4}, "mouth": {"id": 8, "fill": "#a31a57"}, "eyebrow": {"id": 4, "fill": "#9a3300"}, "eyeglasses": {"id": 0}}. Make sure to edit the fill colors of the returned JSON object to match the description of the user. You can set an ID of 0 for optional types like beard, eyebrows, eyeglasses and hair. The body fill refers to the skintone.'
                 ],
                 [
                     'role' => 'user',
@@ -31,6 +44,17 @@ class AICharacterGeneratorController extends Controller
             ]
         ]);
 
-        return response()->json($response->json());
+        $response = $response->json();
+        if ($response['choices']) {
+            return response()->json([
+                'success' => true,
+                'data' => $response['choices'][0]['message']['content']
+            ]);
+        }
+
+        return response()->json([
+            'success' => false,
+            'message' => 'Sorry, couldn\'t generate your Avatar.'
+        ]);
     }
 }
