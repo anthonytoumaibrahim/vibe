0db4cca78e99958208d02f447bffc1bdb73e70de
atian155 <atianthony@outlook.com>
2024-05-08 00:59:36 +0300

    (Frontend) Add Achievements Images

--- frontend/app/(user)/profile/[[...username]]/components/Badges/index.tsx
+++ frontend/app/(user)/profile/[[...username]]/components/Badges/index.tsx
@@ -1,7 +1,25 @@
+import Image from "next/image";
+
 const Badges = ({ badgesData }) => {
   return (
     <div className="p-4 rounded-lg bg-slate-100 dark:bg-black">
       <h3 className="text-center">Badges</h3>
+      <div className="grid grid-cols-3">
+        {badgesData.map((badge) => {
+          const { id, details } = badge;
+
+          return (
+            <div key={id} className="">
+              <Image
+                src={`/images/badges/${details?.slug}.svg`}
+                width={70}
+                height={70}
+                alt={details?.name}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
