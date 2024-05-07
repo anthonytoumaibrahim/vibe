279bbbf9d51629ec72d2d134d0e485db4ad12cb5
atian155 <atianthony@outlook.com>
2024-05-08 01:46:58 +0300

    (Frontend) Add Premium Page

---
+++ frontend/app/(user)/premium/page.tsx
@@ -0,0 +1,45 @@
+import Button from "@/components/Button";
+import Image from "next/image";
+import PremiumBullet from "./components/svg/premium_bullet.svg";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Premium – Vibe",
+};
+
+const Premium = () => {
+  return (
+    <div className="flex gap-4 items-start">
+      <div className="relative w-1/2 h-[720px]">
+        <Image src="/images/premium_lady.svg" fill alt="" />
+      </div>
+      <div className="space-y-4">
+        <h1 className="bg-gradient-to-t from-premium-800 to-premium-400 text-transparent bg-clip-text uppercase text-6xl">
+          Premium
+        </h1>
+        <h3>
+          Become a{" "}
+          <span className="bg-gradient-to-t from-premium-800 to-premium-400 text-transparent bg-clip-text uppercase font-bold">
+            Premium
+          </span>{" "}
+          Member today
+        </h3>
+        <ul className="space-y-2">
+          <li className="flex items-center gap-2">
+            <PremiumBullet width={24} />
+            Unlock more items for your Avatar
+          </li>
+          <li className="flex items-center gap-2">
+            <PremiumBullet width={24} />
+            Boost the Visibility of your Posts on Other Users&apos; Pages
+          </li>
+        </ul>
+        <Button variant="gradient" color="premium">
+          Upgrade Now
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Premium;
