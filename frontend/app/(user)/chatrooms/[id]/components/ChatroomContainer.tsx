e5eba5a11a7f01f514bad1c358894118a32cb29f
atian155 <atianthony@outlook.com>
2024-05-08 12:56:54 +0300

    (Frontend) Add 2D Characters to Chatrooms

--- frontend/app/(user)/chatrooms/[id]/components/ChatroomContainer.tsx
+++ frontend/app/(user)/chatrooms/[id]/components/ChatroomContainer.tsx
@@ -4,6 +4,7 @@ import Echo from "laravel-echo";
 import Pusher from "pusher-js";
 import { useEffect, useState } from "react";
 import { getParticipant, joinChatroom, sendMessage } from "../../actions";
+import Character from "@/app/(user)/2d_components/Character";
 
 interface ChatroomContainerProps {
   id: number;
@@ -13,11 +14,13 @@ const pusherKey = process.env.NEXT_PUBLIC_PUSHER_KEY ?? "";
 const pusherCluster = process.env.NEXT_PUBLIC_PUSHER_CLUSTER ?? "";
 
 const ChatroomContainer = ({ id }: ChatroomContainerProps) => {
-  const [participants, setParticipants] = useState([]);
+  const [participants, setParticipants] = useState<Array<any>>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
-  const get2DCharacter = async (id: number) => {
-    const res = await getParticipant(id);
+  const handleChatPresence = async (userId: number) => {
+    const res = await getParticipant(userId);
+    setParticipants([...participants, res]);
     return res;
   };
 
@@ -33,24 +36,34 @@ const ChatroomContainer = ({ id }: ChatroomContainerProps) => {
     channel.bind("chatroom-message", function (data) {
       console.log(JSON.stringify(data));
     });
-    channel.bind("chatroom-presence", async (data) => {
-      const { id, username } = data;
+    channel.bind("chatroom-presence", (data) => {
+      const { id } = data;
       if (id) {
-        const ch = await get2DCharacter(id);
-        console.log(ch);
+        handleChatPresence(id);
       }
     });
 
     channel.bind("pusher:subscription_succeeded", () => {
-      console.log("connection succeeded!");
       setIsLoading(false);
       joinChatroom(id);
     });
+
+    channel.bind("pusher:subscription_error", (error) => {
+      setIsError(true);
+    });
+
+    return () => {
+      channel.unbind("pusher:subscription_succeeded");
+    };
   }, []);
 
   return (
-    <div className="w-full h-[720px] bg-slate-500 rounded-lg">
-      <button onClick={() => sendMsg()}>send</button>
+    <div className="w-full h-[720px] bg-slate-500 rounded-lg relative overflow-hidden">
+      {!isError &&
+        participants?.map((user) => {
+          const { id, username, character } = user;
+          return <Character key={id} data={character} />;
+        })}
     </div>
   );
 };
