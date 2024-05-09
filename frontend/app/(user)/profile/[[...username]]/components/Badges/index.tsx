a9cf0ac7b77af6e36dc285df819eb92ebe3b1ccc
atian155 <atianthony@outlook.com>
2024-05-09 18:35:23 +0300

    (Frontend) Add Premium Badge

--- frontend/app/(user)/profile/[[...username]]/components/Badges/index.tsx
+++ frontend/app/(user)/profile/[[...username]]/components/Badges/index.tsx
@@ -18,6 +18,7 @@ const Badges = ({ badgesData }) => {
                 width={64}
                 height={64}
                 alt={details?.name}
+                className="w-auto"
               />
               <div className="absolute translate-y-3 top-full left-1/2 -translate-x-1/2 w-max max-w-[320px] bg-gradient-to-t from-primary-100 via-primary-50 to-white dark:from-gray-950 dark:to-slate-800 p-4 rounded-lg shadow-lg flex-col gap-2 before:w-0 before:h-0 before:absolute before:bottom-full before:left-1/2 before:-translate-x-1/2 before:border-l-[8px] before:border-l-transparent before:border-b-[10px] before:border-b-white dark:before:border-b-slate-800 before:border-r-[8px] before:border-r-transparent hidden group-hover:flex text-center">
                 <h4>{details?.name}</h4>
