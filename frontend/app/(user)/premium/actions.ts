de4bdd9028af6577b1cb0176d5b7204b8f798e90
atian155 <atianthony@outlook.com>
2024-05-09 18:18:58 +0300

    (Frontend) Add Become Premium Server Action

--- frontend/app/(user)/premium/actions.ts
+++ frontend/app/(user)/premium/actions.ts
@@ -1,9 +1,19 @@
 import { sendRequest } from "@/app/actions";
+import { revalidatePath } from "next/cache";
 
 export async function getCheckoutUrl() {
   const response = await sendRequest({
     method: "GET",
     url: "/user/premium-checkout",
+  });
+  return response;
+}
+
+// TODO: For local testing only
+export async function becomePremium() {
+  const response = await sendRequest({
+    method: "GET",
+    url: "/user/checkout-success",
   });
   return response;
 }
---
+++ frontend/app/(user)/premium/thank-you/page.tsx
@@ -0,0 +1,28 @@
+import Button from "@/components/Button";
+import VC from "../../components/VC";
+import { becomePremium } from "../actions";
+
+const ThankYou = async () => {
+  const res = await becomePremium();
+
+  return (
+    <div className="text-center space-y-4">
+      <h1>
+        You are now{" "}
+        <span className="bg-gradient-to-t from-premium-800 to-premium-400 text-transparent bg-clip-text uppercase">
+          Premium
+        </span>
+      </h1>
+      <h4 className="flex justify-center items-center gap-2">
+        Thank you for your purchase! You are now a Premium membership.{" "}
+        <VC balance={1000} className="inline-flex" /> were added to your
+        account.
+      </h4>
+      <Button href="/" className="inline-block">
+        Back to Home
+      </Button>
+    </div>
+  );
+};
+
+export default ThankYou;
