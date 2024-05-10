8cd6fcaf9df91a846748e51a62838064167a3ac6
atian155 <atianthony@outlook.com>
2024-05-10 16:47:14 +0300

    (Frontend) Add Participants Count to Chatroom Component

--- frontend/app/(user)/chatrooms/components/Room.tsx
+++ frontend/app/(user)/chatrooms/components/Room.tsx
@@ -1,18 +1,23 @@
 import Button from "@/components/Button";
 import Image from "next/image";
 import Link from "next/link";
+import { FaUserGroup } from "react-icons/fa6";
 
 interface RoomProps {
   id: number;
   name: string;
   username: string;
+  participants_count: number;
 }
 
-const Room = ({ id, name, username }: RoomProps) => {
+const Room = ({ id, name, username, participants_count = 0 }: RoomProps) => {
   return (
     <div className="rounded-lg overflow-hidden bg-slate-200 dark:bg-slate-900">
       <div className="h-44 relative">
         <Image src="/images/chatrooms/bg1.webp" fill sizes="480px" alt="" />
+        <p className="text-white absolute top-4 right-4 flex items-center gap-2">
+          <FaUserGroup size={24} /> {participants_count}
+        </p>
       </div>
       <div className="p-4 flex justify-between items-center">
         <div className="space-y-2">
--- frontend/app/(user)/chatrooms/components/Rooms.tsx
+++ frontend/app/(user)/chatrooms/components/Rooms.tsx
@@ -4,8 +4,16 @@ const Rooms = ({ rooms }) => {
   return (
     <div className="grid grid-cols-3 gap-10">
       {rooms?.map((room) => {
-        const { id, name, host } = room;
-        return <Room key={id} name={name} id={id} username={host?.username} />;
+        const { id, name, host, participants_count } = room;
+        return (
+          <Room
+            key={id}
+            name={name}
+            id={id}
+            username={host?.username}
+            participants_count={participants_count}
+          />
+        );
       })}
     </div>
   );
