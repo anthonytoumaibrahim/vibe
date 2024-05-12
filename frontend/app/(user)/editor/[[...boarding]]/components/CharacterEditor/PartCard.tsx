0defda7e778edb9149eab7a56e16868aa2073f39
atian155 <atianthony@outlook.com>
2024-05-12 11:54:25 +0300

    (Frontend) Fix Headless UI Menu Components

--- frontend/app/(user)/editor/[[...boarding]]/components/CharacterEditor/PartCard.tsx
+++ frontend/app/(user)/editor/[[...boarding]]/components/CharacterEditor/PartCard.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Menu } from "@headlessui/react";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import { FaPalette } from "react-icons/fa6";
 import { useAppDispatch, useAppSelector } from "@/app/lib/store";
 
@@ -108,7 +108,7 @@ const PartCard = ({
               {children}
             </button>
             {colors && (
-              <Menu.Button
+              <MenuButton
                 className={`absolute top-2 right-2 ${
                   open
                     ? "text-primary-main"
@@ -117,11 +117,11 @@ const PartCard = ({
                 tabIndex={0}
               >
                 <FaPalette size={24} />
-              </Menu.Button>
+              </MenuButton>
             )}
-            <Menu.Items className="absolute left-0 top-full z-20 bg-white dark:bg-slate-700 shadow-lg shadow-black/20 p-2 rounded w-full flex items-center justify-center flex-wrap gap-1 before:w-0 before:h-0 before:absolute before:bottom-full before:left-1/2 before:-translate-x-1/2 before:border-l-[8px] before:border-l-transparent before:border-b-[10px] before:border-b-white dark:before:border-b-slate-700 before:border-r-[8px] before:border-r-transparent">
+            <MenuItems className="absolute left-0 top-full z-20 bg-white dark:bg-slate-700 shadow-lg shadow-black/20 p-2 rounded w-full flex items-center justify-center flex-wrap gap-1 before:w-0 before:h-0 before:absolute before:bottom-full before:left-1/2 before:-translate-x-1/2 before:border-l-[8px] before:border-l-transparent before:border-b-[10px] before:border-b-white dark:before:border-b-slate-700 before:border-r-[8px] before:border-r-transparent">
               {colors?.map((color, colorIndex) => (
-                <Menu.Item
+                <MenuItem
                   as="button"
                   key={colorIndex}
                   className={`w-6 h-6 rounded-sm relative overflow-hidden before:w-6 before:h-6 before:absolute before:-top-3 before:-right-3 before:bg-white before:rounded-full before:blur-sm before:opacity-0 hover:before:opacity-100 before:mix-blend-overlay ${
@@ -129,9 +129,9 @@ const PartCard = ({
                   }`}
                   style={{ backgroundColor: color }}
                   onClick={() => handleUpdate({ id: id, fill: color })}
-                ></Menu.Item>
+                ></MenuItem>
               ))}
-            </Menu.Items>
+            </MenuItems>
           </>
         )}
       </Menu>
