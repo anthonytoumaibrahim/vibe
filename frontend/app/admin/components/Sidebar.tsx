8a5702a829b31f4f1c2526b715f30ede549e2a08
atian155 <atianthony@outlook.com>
2024-05-12 13:04:47 +0300

    (Frontend) Add Admin Layout and Sidebar

---
+++ frontend/app/admin/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import Logo from "@/components/Logo";
+
+const Sidebar = () => {
+  return (
+    <nav className="w-full min-h-screen max-w-sm border-r-2 border-slate-300">
+      <Logo />
+    </nav>
+  );
+};
+
+export default Sidebar;
--- frontend/app/admin/layout.tsx
+++ frontend/app/admin/layout.tsx
@@ -1,5 +1,11 @@
-const AdminLayout = () => {
-  return <div>AdminLayout</div>;
+import Sidebar from "./components/Sidebar";
+
+const AdminLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full min-h-screen flex">
+      <Sidebar />
+    </div>
+  );
 };
 
 export default AdminLayout;
