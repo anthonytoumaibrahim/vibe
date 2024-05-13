b3f6b6c5d47dd5a8e5b6061c77eae25c57e6b382
atian155 <atianthony@outlook.com>
2024-05-13 11:38:13 +0300

    (Backend) Update Users Table

--- frontend/app/admin/users/components/UsersTable.tsx
+++ frontend/app/admin/users/components/UsersTable.tsx
@@ -1,28 +1,64 @@
 const UsersTable = ({ data = [] }) => {
   return (
-    <table className="table-auto w-full border-collapse border border-slate-400">
-      <thead className="bg-slate-400">
-        <tr>
-          <th>ID</th>
-          <th>Username</th>
-          <th>Email</th>
-          <th>Join Date</th>
-        </tr>
-      </thead>
-      <tbody className="bg-white">
-        {data?.map((u) => {
-          const { id, username, email, created_at } = u;
-          return (
-            <tr key={id}>
-              <td>{id}</td>
-              <td>{username}</td>
-              <td>{email}</td>
-              <td>{created_at}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <div className="flex flex-col">
+      <div className="-m-1.5 overflow-x-auto">
+        <div className="p-1.5 min-w-full inline-block align-middle">
+          <div className="overflow-hidden">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead>
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+                  >
+                    ID
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+                  >
+                    Username
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+                  >
+                    Email
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase"
+                  >
+                    Action
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-200">
+                {data?.map((u) => {
+                  const { id, username, email, created_at } = u;
+                  return (
+                    <tr key={id}>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
+                        {id}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
+                        {username}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
+                        {email}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
+                        {created_at}
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
