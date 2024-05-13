5e75ad64f7ecbb00afdcdf95a8d26fca17f360d9
atian155 <atianthony@outlook.com>
2024-05-13 10:31:36 +0300

    (Frontend) Add Footer to Layout

--- frontend/app/(user)/layout.tsx
+++ frontend/app/(user)/layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import UserHeader from "./components/UserHeader";
+import Footer from "@/components/Footer";
 
 const UserLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <UserHeader />
       <main className="container">{children}</main>
+      <Footer />
     </>
   );
 };
--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,8 +1,12 @@
 import Logo from "./Logo";
 
-const Footer = () => {
+interface FooterProps {
+  className?: string;
+}
+
+const Footer = ({ className = "" }: FooterProps) => {
   return (
-    <footer className="bg-black text-white py-10 mt-auto">
+    <footer className={`bg-black text-white py-10 mt-10`}>
       <div className="container">
         <Logo fill="#ffffff" width={88} />
         <p className="mt-2">&copy; 2024 - Vibe</p>
