59a374f1627e12417bac71b1cfe9675876101c4e
atian155 <atianthony@outlook.com>
2024-05-14 17:27:19 +0300

    (Backend) Add User Unit Test

---
+++ backend/tests/Feature/UserTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_logged_out_user_info(): void
+    {
+        $response = $this->json("GET", "/api/user/info");
+
+        $response->assertStatus(401);
+
+        $response->assertJson([
+            'message' => 'Unauthenticated.'
+        ]);
+    }
+}
