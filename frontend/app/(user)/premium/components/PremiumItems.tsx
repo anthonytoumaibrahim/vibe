8f0d58c22c07e28b922673ad268d23f67da5a3fa
atian155 <atianthony@outlook.com>
2024-05-14 20:53:33 +0300

    (Frontend) Fix Premium Page Items Thumbnails

--- frontend/app/(user)/premium/components/PremiumItems.tsx
+++ frontend/app/(user)/premium/components/PremiumItems.tsx
@@ -9,10 +9,10 @@ const PremiumItems = ({ items = [] }) => {
         className="group size-32 z-0 rounded-lg relative overflow-hidden transition-colors duration-200 before:w-1/2 before:h-1/2 before:absolute before:-right-6 before:-top-6 before:rounded-full before:filter before:blur-lg after:w-full after:h-full after:rounded-full after:absolute after:top-0 after:left-0 after:transition after:filter after:blur-xl before:-z-[1] after:-z-[2] bg-premium-500 hover:bg-premium-700 before:bg-premium-200 after:bg-premium-400 before:opacity-100 after:opacity-100"
       >
         <Image
-          src={`/images/2d_thumbs/${type}/${id}.svg`}
-          fill
-          sizes="100%"
-          className="object-contain"
+          src={`/images/2d_thumbs/${type}/${id}.webp`}
+          width={180}
+          height={180}
+          className="object-contain h-full"
           alt=""
         />
       </div>
