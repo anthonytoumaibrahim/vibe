ae0d65d0a1c8312378399ff59a620e2ca3d91e40
atian155 <atianthony@outlook.com>
2024-05-15 13:15:52 +0300

    (Frontend) Fix Avatar Dropdown Position on Header

--- frontend/app/(user)/components/UserHeader/HeaderAvatar.tsx
+++ frontend/app/(user)/components/UserHeader/HeaderAvatar.tsx
@@ -20,7 +20,10 @@ const HeaderAvatar = ({
         <Avatar url={avatar ? avatar : null} isPremium={is_premium} />
       </PopoverButton>
 
-      <PopoverPanel className="absolute translate-y-2 top-full left-1/2 -translate-x-1/2 w-max z-50 bg-white dark:bg-black py-4 px-6 rounded-lg shadow-lg flex flex-col gap-2 before:w-0 before:h-0 before:absolute before:bottom-full before:left-1/2 before:-translate-x-1/2 before:border-l-[8px] before:border-l-transparent before:border-b-[10px] before:border-b-white dark:before:border-b-black before:border-r-[8px] before:border-r-transparent">
+      <PopoverPanel
+        anchor="bottom"
+        className="absolute translate-y-2 w-max z-50 bg-white dark:bg-black py-4 px-6 rounded-lg shadow-lg flex flex-col gap-2"
+      >
         <p className="text-center font-bold">{username}</p>
         <ThemeToggler />
         <LogoutButton />
