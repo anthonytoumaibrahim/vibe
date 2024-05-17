a79c38d58df7f2ac8ad4e8307fc3a8417a45f15f
atian155 <atianthony@outlook.com>
2024-05-17 11:46:15 +0300

    (Frontend) Reset font size of Admin Panel Tables

--- frontend/app/admin/components/TableData.tsx
+++ frontend/app/admin/components/TableData.tsx
@@ -1,6 +1,6 @@
 const TableData = ({ children }: { children: React.ReactNode }) => {
   return (
-    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-300">
+    <td className="px-6 py-4 whitespace-nowrap text-gray-800 dark:text-gray-300">
       {children}
     </td>
   );
