c64bdf1d5b33d91f06b2bbfe308447c2ebd99cef
atian155 <atianthony@outlook.com>
2024-05-18 19:19:16 +0300

    (Backend) Modify Feed Controller to Return Daily Top Rated Posts

--- backend/app/Http/Controllers/User/FeedController.php
+++ backend/app/Http/Controllers/User/FeedController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\User;
 
-use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 
 class FeedController extends Controller
@@ -13,7 +14,25 @@ class FeedController extends Controller
     public function getFeed()
     {
         $members = User::limit(5)->orderBy('created_at', 'DESC')->get();
-        $posts = Post::where('user_id', '!=', Auth::id())->withCount('likes')->orderBy('likes_count', 'DESC')->orderBy('created_at', 'DESC')->paginate(5);
+
+        $today = Carbon::today();
+
+        $posts = Post::where('created_at', '>=', $today)
+            ->where('user_id', '!=', Auth::id())
+            ->withCount('likes')
+            ->orderBy('likes_count', 'DESC')
+            ->orderBy('created_at', 'DESC')
+            ->paginate(5);
+
+
+        if ($posts->isEmpty()) {
+            $posts = Post::where('created_at', '<', $today)
+                ->where('user_id', '!=', Auth::id())
+                ->withCount('likes')
+                ->orderBy('likes_count', 'DESC')
+                ->orderBy('created_at', 'DESC')
+                ->paginate(5);
+        }
 
         return response()->json([
             'latest_members' => $members,
